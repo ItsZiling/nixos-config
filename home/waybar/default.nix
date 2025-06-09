5b4a72038006c9a75b1165ee59a688b82fec6170
ziling <jasonchen7766@gmail.com>
2025-06-08 22:36:18 -0400

    move stuff around, configure some of waybar

--- home/waybar/default.nix
+++ home/waybar/default.nix
@@ -1,8 +1,40 @@
-
-
 {
-  programs.waybar = { 
+  programs.waybar = {
     enable = true;
     style = ./style.css;
+    settings = [
+      {
+        layer = "top";
+        position = "top";
+        height = 40;
+
+        modules-left = [
+          "hyprland/workspaces"
+          "hyprland/mode"
+          "hyprland/window"
+        ];
+
+        modules-center = [
+          "clock"
+        ];
+
+        modules-right = [
+          "custom/recorder"
+          "tray"
+          "pulseaudio"
+          "backlight"
+          "temperature"
+          "cpu"
+          "memory"
+          "battery"
+          "network"
+          "custom/powermenu"
+        ];
+
+        "hyprland/mode" = { 
+           "format" = " {}";
+        };
+      }
+    ];
   };
 }
