ea55e8759c873ea943ce5f52352dac598c400688
ziling <jasonchen7766@gmail.com>
2025-10-06 06:41:46 -0400

    commiting

--- home/core/hypr/hyprland.nix
+++ home/core/hypr/hyprland.nix
@@ -27,10 +27,90 @@
       "$browser" = "firefox";
       "$fileManager" = "$terminal -e yazi";
       "$menu" = "wofi --show drun";
+      exec = ["hyprctl dispatch submap global"];
 
-      exec-once = [
-        "waybar"
-        "swaync"
+      submap = "global";
+
+      #Caelestia Shell
+      bindi = [
+        "$mainMod, Super_L, global, caelestia:launcher"
+      ];
+
+      bindin = [
+        "$mainMod, catchall, global, caelestia:launcherInterrupt"
+        "$mainMod, mouse:272, global, caelestia:launcherInterrupt"
+        "$mainMod, mouse:273, global, caelestia:launcherInterrupt"
+        "$mainMod, mouse:274, global, caelestia:launcherInterrupt"
+        "$mainMod, mouse:275, global, caelestia:launcherInterrupt"
+        "$mainMod, mouse:276, global, caelestia:launcherInterrupt"
+        "$mainMod, mouse:277, global, caelestia:launcherInterrupt"
+        "$mainMod, mouse_up, global, caelestia:launcherInterrupt"
+        "$mainMod, mouse_down, global, caelestia:launcherInterrupt"
+      ];
+
+      bind = [
+        "$mainMod, RETURN, exec, $terminal"
+        "$mainMod, B, exec, $browser"
+        "$mainMod, C, killactive"
+        "$mainMod, M, exit"
+        "$mainMod, E, exec, $fileManager"
+        "$mainMod, V, togglefloating"
+        "$mainMod, P, pseudo"
+        "$mainMod, J, togglesplit"
+        "$mainMod, N, exec, swaync-client -t -sw"
+        "$mainMod, F, FULLSCREEN"
+
+        # Hyprshot keybinds
+        "CTRL, PRINT, exec, hyprshot -m window"
+        ", PRINT, exec, hyprshot -m output"
+        "CTRL_SHIFT, PRINT, exec, hyprshot -m region"
+
+        # Hyprlock
+        "$mainMod, L, exec, hyprlock"
+
+        "$mainMod, left, movefocus, l"
+        "$mainMod, right, movefocus, r"
+        "$mainMod, up, movefocus, u"
+        "$mainMod, down, movefocus, d"
+
+        "$mainMod, 1, workspace, 1"
+        "$mainMod, 2, workspace, 2"
+        "$mainMod, 3, workspace, 3"
+        "$mainMod, 4, workspace, 4"
+        "$mainMod, 5, workspace, 5"
+        "$mainMod, 6, workspace, 6"
+        "$mainMod, 7, workspace, 7"
+        "$mainMod, 8, workspace, 8"
+        "$mainMod, 9, workspace, 9"
+        "$mainMod, 0, workspace, 10"
+
+        "$mainMod SHIFT, 1, movetoworkspace, 1"
+        "$mainMod SHIFT, 2, movetoworkspace, 2"
+        "$mainMod SHIFT, 3, movetoworkspace, 3"
+        "$mainMod SHIFT, 4, movetoworkspace, 4"
+        "$mainMod SHIFT, 5, movetoworkspace, 5"
+        "$mainMod SHIFT, 6, movetoworkspace, 6"
+        "$mainMod SHIFT, 7, movetoworkspace, 7"
+        "$mainMod SHIFT, 8, movetoworkspace, 8"
+        "$mainMod SHIFT, 9, movetoworkspace, 9"
+        "$mainMod SHIFT, 0, movetoworkspace, 10"
+
+        "$mainMod, mouse_down, workspace, e+1"
+        "$mainMod, mouse_up, workspace, e-1"
+      ];
+
+      bindm = [
+        "$mainMod, mouse:272, movewindow"
+        "$mainMod, mouse:273, resizewindow"
+      ];
+
+      bindel = [
+        ",XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"
+        ",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+        ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+        ",XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+        ",XF86MonBrightnessUp, exec, brightnessctl -e4 -n2 set 5%+"
+        ",XF86MonBrightnessDown, exec, brightnessctl -e4 -n2 set 5%-"
       ];
 
       general = {
@@ -94,73 +174,6 @@
           "workspacesOut, 1, 1.94, almostLinear, fade"
         ];
       };
-
-      bind = [
-        "$mainMod, RETURN, exec, $terminal"
-        "$mainMod, B, exec, $browser"
-        "$mainMod, C, killactive"
-        "$mainMod, M, exit"
-        "$mainMod, E, exec, $fileManager"
-        "$mainMod, V, togglefloating"
-        "$mainMod, R, exec, $menu"
-        "$mainMod, P, pseudo"
-        "$mainMod, J, togglesplit"
-        "$mainMod, n, exec, swaync-client -t -sw"
-        "$mainMod, F, FULLSCREEN"
-
-        # Hyprshot keybinds
-        "CTRL, PRINT, exec, hyprshot -m window"
-        ", PRINT, exec, hyprshot -m output"
-        "CTRL_SHIFT, PRINT, exec, hyprshot -m region"
-
-        # Hyprlock
-        "$mainMod, L, exec, hyprlock"
-
-        "$mainMod, left, movefocus, l"
-        "$mainMod, right, movefocus, r"
-        "$mainMod, up, movefocus, u"
-        "$mainMod, down, movefocus, d"
-
-        "$mainMod, 1, workspace, 1"
-        "$mainMod, 2, workspace, 2"
-        "$mainMod, 3, workspace, 3"
-        "$mainMod, 4, workspace, 4"
-        "$mainMod, 5, workspace, 5"
-        "$mainMod, 6, workspace, 6"
-        "$mainMod, 7, workspace, 7"
-        "$mainMod, 8, workspace, 8"
-        "$mainMod, 9, workspace, 9"
-        "$mainMod, 0, workspace, 10"
-
-        "$mainMod SHIFT, 1, movetoworkspace, 1"
-        "$mainMod SHIFT, 2, movetoworkspace, 2"
-        "$mainMod SHIFT, 3, movetoworkspace, 3"
-        "$mainMod SHIFT, 4, movetoworkspace, 4"
-        "$mainMod SHIFT, 5, movetoworkspace, 5"
-        "$mainMod SHIFT, 6, movetoworkspace, 6"
-        "$mainMod SHIFT, 7, movetoworkspace, 7"
-        "$mainMod SHIFT, 8, movetoworkspace, 8"
-        "$mainMod SHIFT, 9, movetoworkspace, 9"
-        "$mainMod SHIFT, 0, movetoworkspace, 10"
-
-        "$mainMod, mouse_down, workspace, e+1"
-        "$mainMod, mouse_up, workspace, e-1"
-      ];
-
-      bindm = [
-        "$mainMod, mouse:272, movewindow"
-        "$mainMod, mouse:273, resizewindow"
-      ];
-
-      bindel = [
-        ",XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"
-        ",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
-        ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
-        ",XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
-        ",XF86MonBrightnessUp, exec, brightnessctl -e4 -n2 set 5%+"
-        ",XF86MonBrightnessDown, exec, brightnessctl -e4 -n2 set 5%-"
-      ];
-
       env = [
         "HYPRSHOT_DIR, Pictures/Screenshots"
         "NIXOS_OZONE_WL, 1"
